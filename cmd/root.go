2ab41ced69ea4cfdc8a79358a0e5315a81a7e6d4
Oscar <46930728+osulli@users.noreply.github.com>
2021-01-02 21:57:11 +0000

    Migrate fully to Cobra & Viper :snake:

--- cmd/root.go
+++ cmd/root.go
@@ -1,83 +1,74 @@
-/*
- Copyright (c) 2020 - 2021 the WolvenKit contributors.
-
- Licensed under the GNU Affero General Public License v3.0 (the "License").
-*/
-
 package cmd
 
 import (
-    "fmt"
-    "github.com/spf13/cobra"
-    "os"
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
 )
 
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-    Use:   "gpm",
-    Short: "A game agnostic mod manager",
-    Long: `A Fast(ish) and Flexible, game agnostic, mod manager built with
+	Use:   "gpm",
+	Short: "A game agnostic mod manager",
+	Long: `A Fast(ish) and Flexible, game agnostic, mod manager built with
                 love by osulli and WolvenKit Devs in Go.
 
                 Source available at https://github.com/WolvenKit/gpm
 
                 Copyright (c) 2020 - 2021 the WolvenKit contributors.
                 Licensed under the GNU Affero General Public License v3.0 (the "License").`,
-    Run: func(cmd *cobra.Command, args []string) {
-        // Do Stuff Here
-    },
 }
 
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-    if err := rootCmd.Execute(); err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        os.Exit(1)
-    }
-}
-
-/*
-
-const VERSION = "0.0.0"
-
-var AUTHORS = []*cli.Author{
-	{Name: "osulli", Email: "https://github.com/osulli"},
-}
-
-var COMMANDS = []*cli.Command{
-	versionCommand(VERSION),
-}
-
-func main() {
-	app := &cli.App{
-		Name:                   "gpm",
-		Version:                VERSION,
-		HelpName:               "help",
-		Usage:                  "A game agnostic mod manager",
-		Authors:                AUTHORS,
-		Commands:               COMMANDS,
-		Compiled:               time.Time{},
-		Copyright:              "Copyright (c) 2020 - 2021 the WolvenKit contributors.\n   Licensed under the GNU Affero General Public License v3.0 (the \"License\").",
-		UseShortOptionHandling: true,
-		EnableBashCompletion:   true,
-		HideHelp:               false,
-		Action:                 nil,
-		CommandNotFound:        nil,
-		OnUsageError:           nil,
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	app.Run(os.Args)
 }
 
-func versionCommand(version string) *cli.Command {
-	command := cli.Command{
-		Name:     "Version",
-		Aliases:  []string{"v", "--version", "-v", "version"},
-		Usage:    "Print the application version",
-		Category: "help",
-		Action: func(context *cli.Context) error {
-			println(VERSION)
-			return nil
-		},
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gpm.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".newApp" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".gpm")
 	}
 
-	return &command
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
 }
-*/
--- go.mod
+++ go.mod
@@ -3,12 +3,19 @@ module github.com/WolvenKit/gpm
 go 1.15
 
 require (
-	github.com/WolvenKit/gpm/internal/gpm/commands v0.0.0
-	github.com/WolvenKit/gpm/internal/gpm/game v0.0.0-00010101000000-000000000000 // indirect
+	github.com/dsnet/compress v0.0.1 // indirect
+	github.com/frankban/quicktest v1.11.3 // indirect
+	github.com/golang/snappy v0.0.2 // indirect
 	github.com/mholt/archiver v3.1.1+incompatible
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/nwaples/rardecode v1.1.0 // indirect
+	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
 	github.com/spf13/cobra v1.1.1
+	github.com/spf13/viper v1.7.0
 	github.com/stretchr/testify v1.6.1
+	github.com/ulikunitz/xz v0.5.9 // indirect
 	github.com/urfave/cli/v2 v2.3.0
+	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 )
 
 replace github.com/WolvenKit/gpm/internal/gpm/commands => ./internal/gpm/commands

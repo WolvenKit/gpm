e2221706e1e7cd2ccb3e07fcec6c42b056cd4ee2
Oscar <46930728+osulli@users.noreply.github.com>
2021-01-02 22:36:23 +0000

    Create version.go

---
+++ cmd/version.go
@@ -0,0 +1,29 @@
+/*
+ Copyright (c) 2020 - 2021 the WolvenKit contributors.
+
+ Licensed under the GNU Affero General Public License v3.0 (the "License").
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package cmd
+
+import (
+    "fmt"
+    "github.com/spf13/cobra"
+)
+
+// TODO - Dynamic version for gpm
+var VERSION = "0.0.0"
+
+var versionCmd = &cobra.Command{
+   Use:   "version",
+   Short: "Print the version",
+   Run: func(cmd *cobra.Command, args []string) {
+       fmt.Println(VERSION)
+   },
+}

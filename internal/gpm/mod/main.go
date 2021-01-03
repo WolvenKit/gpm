8df57b25fe0bca16db0b33e9623d5ce58a8a2169
Oscar <46930728+osulli@users.noreply.github.com>
2021-01-03 13:35:53 +0000

    Add ability for mods to read their TOML manifest

--- internal/gpm/mod/main.go
+++ internal/gpm/mod/main.go
@@ -2,11 +2,21 @@
  Copyright (c) 2020 - 2021 the WolvenKit contributors.
 
  Licensed under the GNU Affero General Public License v3.0 (the "License").
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 */
 
 package mod
 
-import 	"github.com/WolvenKit/gpm/internal/gpm/game"
+import (
+    "fmt"
+    "github.com/spf13/viper"
+    "go.uber.org/zap"
+)
 
 type Mod struct {
     Creator string
@@ -18,12 +28,35 @@ type Mod struct {
     WebsiteURL string
     Dependencies []string
     Tags []string
-    InstallStrategies []game.InstallStrategy
+    InstallStrategies []string
     ExtraData []string
 }
 
-func InitMod() Mod{
-    m := Mod{}
+func InitMod(logger *zap.SugaredLogger, directory string) *Mod {
+    viper.AddConfigPath(directory)
+    viper.SetConfigName("manifest")
+    viper.SetConfigType("toml")
 
+    err := viper.ReadInConfig()
+    if err != nil {
+        panic(fmt.Errorf("Fatal error config file: %s \n", err))
+    }
+
+    m := new(Mod)
     return m
 }
+
+func (m *Mod) ReadModConfiguration()  {
+    m.Creator = viper.GetString("creator")
+    m.Identifier = viper.GetString("identifier")
+    m.Version = viper.GetString("version")
+    m.DisplayName = viper.GetString("display_name")
+    m.Description = viper.GetString("description")
+    m.License = viper.GetString("license")
+    m.WebsiteURL = viper.GetString("website_url")
+    m.Dependencies = viper.GetStringSlice("dependencies")
+    m.Tags = viper.GetStringSlice("tags")
+    m.InstallStrategies = viper.GetStringSlice("install_strategies")
+    m.ExtraData = viper.GetStringSlice("extra_data")
+}
+
--- test/user_flow_test.go
+++ test/user_flow_test.go
@@ -2,40 +2,78 @@
  Copyright (c) 2020 - 2021 the WolvenKit contributors.
 
  Licensed under the GNU Affero General Public License v3.0 (the "License").
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 */
 
 package test
 
 import (
-    "github.com/WolvenKit/gpm/cmd"
-    "github.com/stretchr/testify/assert"
-    "testing"
+	"github.com/WolvenKit/gpm/cmd"
+	"github.com/WolvenKit/gpm/internal/gpm/mod"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-
+// Test downloads go to desired directory
 func TestDownloadMod(t *testing.T) {
-    tmp := createSandbox(false)
-    logger := initLogging()
+	tmp := createSandbox(false)
+	logger := initLogging()
 
-    url := "https://cybermods.net/package/download/osulli/BraindanceProtocol/0.4.0/"
-    identifier := "braindance_protocol"
-    fileType := ".zip"
+	// Mock user CLI input
+	i := new(cmd.Input)
+	i.Url = "https://cybermods.net/package/download/osulli/BraindanceProtocol/0.4.0/"
+	i.Identifier = "braindance_protocol"
+	i.FileType = ".zip"
 
-    _, archivePath := cmd.DownloadMod(logger, url, tmp, identifier, fileType)
+	_, archivePath := cmd.DownloadMod(logger, tmp, i)
 
-    assert.FileExists(t, archivePath)
+	assert.FileExists(t, archivePath)
 }
 
+// Tests mod Configuration is read correctly
+func TestReadModConfiguration(t *testing.T) {
+	logger := initLogging()
+
+	m := mod.InitMod(logger, "mocks/example_cet_mod")
+	m.ReadModConfiguration()
+	logger.Debug(m.Creator)
+
+	assert.Equal(t, "WolvenKit", m.Creator)
+	assert.Equal(t, "braindance-protocol", m.Identifier)
+	assert.Equal(t, "0.0.0", m.Version)
+	assert.Equal(t, "Braindance Protocol", m.DisplayName)
+	assert.Equal(t, "A collection of LUA scripts to modify your Cyberpunk 2077 experience", m.Description)
+	assert.Equal(t, "GNU v3", m.License)
+	assert.Equal(t, "https://github.com/WolvenKit/BraindanceProtocol/", m.WebsiteURL)
+	assert.Equal(t, []string{""}, m.Dependencies)
+	assert.Equal(t, []string{""}, m.Tags)
+	assert.Equal(t, []string{"CET"}, m.InstallStrategies)
+	assert.Equal(t, []string{""}, m.ExtraData)
+}
+
+//// Tests mod Install follows install strategy
 //func TestInstallMod(t *testing.T){
-//    tmp := createSandbox()
+//  tmp := createSandbox(false)
+//  logger := initLogging()
 //
-//    commands.GetConfiguration("cyberpunk", "cybermods", "braindance-protocol", "0.0.0")
-//    mod := cmd.InstallMod(archivePath, tmp, identifier)
-//    assert.DirExists(t, mod)
+//  // commands.GetConfiguration("cyberpunk", "cybermods", "braindance-protocol", "0.0.0")
+//  mod := cmd.InstallMod(logger, archivePath, tmp, identifier)
+//  assert.DirExists(t, mod)
 //}
 
-func InstallNoDirectories(){
-    // Checks CET not existing handled etc.
+// Ensure scenario where mod archive cannot be found is handled
+func UnarchiveMissingArchive() {
+	// Checks CET not existing handled etc.
+}
+
+// Ensure scenario is handled if required directories do not exist
+func InstallToInvalidDirectory() {
+	// Checks CET not existing handled etc.
 }
 
 func InstallMod(t *testing.T) {
